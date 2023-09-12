d0bad13035ceb4d6ee04f42e9b1f91f4100f2289
20024094 <20024094@10.5.144.235>
2023-09-12 12:01:18 +0100

    Commit

---
+++ src/europecapital/EuropeCapital.java
@@ -0,0 +1,13 @@
+package europecapital;
+
+/**
+ *
+ * @author 20024094
+ */
+public class EuropeCapital {
+
+    public static void main(String[] args) {
+        System.out.println("European capital is Brussels");
+    }
+    
+}
